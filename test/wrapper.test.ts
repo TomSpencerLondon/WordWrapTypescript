93b7d2aff2ed197c4682afcc32ef1223b5a825cd
Tom <thomas.spencer@codurance.com>
2020-11-12 18:58:48 +0000

    Initial commit

---
+++ test/wrapper.test.ts
@@ -0,0 +1,11 @@
+import {wrap} from "../src/wrapper";
+
+describe("Wrapper", () => {
+  it("returns empty string", () => {
+    expect(wrap("", 1)).toEqual("");
+  });
+
+  it("returns string shorter than column", () => {
+    expect(wrap("this", 10)).toEqual("this");
+  });
+});
